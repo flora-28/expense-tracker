64f3f972c90b7ebe310072a3e447fc5bb0375e51
Flora Peng <berry1003@outlook.com>
2021-02-04 14:26:32 +0800

    feat: add create todo function

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
-const Record = require('./models/record')
+const bodyParser = require('body-parser')
 
+const Record = require('./models/record')
 
 const app = express()
 const port = 3000
@@ -20,10 +21,23 @@ db.once('open', () => {
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
+app.use(bodyParser.urlencoded({ extended: true }))
+
 app.get('/', (req, res) => {
   Record.find()
     .lean()
     .then(records => res.render('index', { records }))
+    .catch(error => console.log(error))
+})
+
+app.get('/records/new', (req, res) => {
+  return res.render('new')
+})
+
+app.post('/records', (req, res) => {
+  const recordItem = req.body
+  return Record.create(recordItem)
+    .then(() => res.redirect('/'))
     .catch(error => console.log(error))
 })
 
