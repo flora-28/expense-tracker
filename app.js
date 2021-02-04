5ff7791efa1da287054b14f55e3a2da6d7682811
Flora Peng <berry1003@outlook.com>
2021-02-04 14:57:09 +0800

    feat: add delete record function

--- app.js
+++ app.js
@@ -61,6 +61,14 @@ app.post('/records/:id/edit', (req, res) => {
     .catch(error => console.log(error))
 })
 
+app.post('/records/:id/delete', (req, res) => {
+  const id = req.params.id
+  return Record.findById(id)
+    .then(record => record.remove())
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
 app.listen(port, () => {
   console.log(`App is running on http://localhost:${port}`)
 })
